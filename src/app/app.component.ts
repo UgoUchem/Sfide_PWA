480149b1be42a54926240f76d49a2b1376392bf1
ugo uchem <ugouchem16@gmail.com>
2025-03-12 09:56:27 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+type Item = {
+  id: number;
+  name: string;
+};
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1>Welcome to {{ title }}!</h1>
 
-    <router-outlet />
+    <button (click)="toggleShow()">Toggle Message</button>
+
+    @if (showMessage()) {
+      <p>Welcome! This is a Progressive Web App built with Angular 19.</p>
+    }
+
+    <ul>
+      @for(item of items(); track item.id){
+      <li>{{ item.name }}</li>
+      }
+    </ul>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'Sfide_PWA';
+  title = 'Sfide PWA';
+
+  protected readonly items: WritableSignal<Item[]> = signal([
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Banana' },
+    { id: 3, name: 'Cherry' },
+  ]);
+
+  protected readonly showMessage:WritableSignal<boolean> = signal(false);
+
+  toggleShow(){
+    this.showMessage.set(!this.showMessage());
+  }
 }
