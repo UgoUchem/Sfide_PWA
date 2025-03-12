921f372d54b5a8b8230986ea93d9c1c631c51e5b
ugo uchem <ugouchem16@gmail.com>
2025-03-12 14:48:00 +0100

    copied challenges.json in src/app/data so it can load challenges when server is down

--- src/app/features/challenge/services/challenge.service.ts
+++ src/app/features/challenge/services/challenge.service.ts
@@ -1,17 +1,25 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, from, Observable, of, timeout } from 'rxjs';
 import { Challenge } from '../models/challenge.model';
+import challengesData from '../../../data/challenges.json'
 @Injectable({
   providedIn: 'root',
 })
 export class ChallengeService {
   private apiUrl = 'http://localhost:3000/challenges';
 
+  private challenges:WritableSignal<Challenge[]> = signal<Challenge[]>(challengesData);
   private readonly http:HttpClient = inject(HttpClient);
 
-  getChallenges():Observable<Challenge[]>{
-    return this.http.get<Challenge[]>(this.apiUrl);
+  getChallenges(): Observable<Challenge[]> {
+    return this.http.get<Challenge[]>(this.apiUrl).pipe(
+      timeout(1000), 
+      catchError(() => {
+        console.warn('Server not available, loading local challenges.json');
+        return of(this.challenges())
+      })
+    );
   }
 
   addChallenge(challenge:Challenge):Observable<Challenge>{
