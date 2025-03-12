e331b5fa72bc49b70fd99e243d130c763fe2648c
ugo uchem <ugouchem16@gmail.com>
2025-03-12 14:52:39 +0100

    made the localstorage store the parse json and load it instantly on next visit

--- src/app/features/challenge/services/challenge.service.ts
+++ src/app/features/challenge/services/challenge.service.ts
@@ -2,34 +2,41 @@ import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, from, Observable, of, timeout } from 'rxjs';
 import { Challenge } from '../models/challenge.model';
-import challengesData from '../../../data/challenges.json'
+import challengesData from '../../../data/challenges.json';
 @Injectable({
   providedIn: 'root',
 })
 export class ChallengeService {
   private apiUrl = 'http://localhost:3000/challenges';
 
-  private challenges:WritableSignal<Challenge[]> = signal<Challenge[]>(challengesData);
-  private readonly http:HttpClient = inject(HttpClient);
+  private challenges: WritableSignal<Challenge[]> =
+    signal<Challenge[]>(challengesData);
+  private readonly http: HttpClient = inject(HttpClient);
 
   getChallenges(): Observable<Challenge[]> {
+    const cachedChallenges = localStorage.getItem('challenges');
+
+    if (cachedChallenges) {
+      return of(JSON.parse(cachedChallenges)); // Load cached data instantly
+    }
     return this.http.get<Challenge[]>(this.apiUrl).pipe(
-      timeout(1000), 
+      timeout(2000),
       catchError(() => {
         console.warn('Server not available, loading local challenges.json');
-        return of(this.challenges())
+        localStorage.setItem('challenges', JSON.stringify(this.challenges()));
+        return of(this.challenges());
       })
     );
   }
 
-  addChallenge(challenge:Challenge):Observable<Challenge>{
-    return this.http.post<Challenge>(this.apiUrl,challenge);
+  addChallenge(challenge: Challenge): Observable<Challenge> {
+    return this.http.post<Challenge>(this.apiUrl, challenge);
   }
-  addChallenges(Challenge:Challenge[]):Observable<Challenge[]>{
-    return this.http.post<Challenge[]>(this.apiUrl,Challenge);
+  addChallenges(Challenge: Challenge[]): Observable<Challenge[]> {
+    return this.http.post<Challenge[]>(this.apiUrl, Challenge);
   }
 
-  deleteChallenge(id:string):Observable<void>{
-    return this.http.delete<void>(`${this.apiUrl}/${id}`)
+  deleteChallenge(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
